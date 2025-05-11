c07091aa7b121c45c808f7038e919f53f09a31e0
RoyalJonesGo <152839662+RoyalJonesGo@users.noreply.github.com>
2025-05-11 23:01:53 +0800

    Create Department.java

---
+++ Department.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Department extends OrganizationComponent{
+    private List<OrganizationComponent> components = new ArrayList<>();
+
+    public Department(String name){
+        super(name);
+    }
+    @Override
+    public void add(OrganizationComponent component){
+        components.add(component);
+    }
+    @Override
+    public void remove(OrganizationComponent component){
+        components.remove(component);
+    }
+    @Override
+    public void displayDetails(String indent){
+        System.out.println(indent + "Department: " + name);
+        for (OrganizationComponent component : components){
+            component.displayDetails(indent + "  ");
+        }
+    }
+    @Override
+    public int getStudentCount(){
+        int total = 0;
+        for (OrganizationComponent component : components){
+            total += component.getStudentCount();
+        }
+        return total;
+    }
+    @Override
+    public double getBudget(){
+        double total = 0;
+        for (OrganizationComponent component : components){
+            total += component.getBudget();
+        }
+        return total;
+    }
+}
